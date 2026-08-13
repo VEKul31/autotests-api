cf784dae92999ad55a6d653a36501d0b04a273f1
VEKul31 <Sev7715@gmail.com>
2026-08-13 14:13:37 +0300

    Знакомство с WebSocket протоколом 2.0

---
+++ websocket_users_client.py
@@ -0,0 +1,16 @@
+import asyncio
+
+import websockets
+
+
+async def client():
+    uri = "ws://localhost:8765"  # Адрес сервера
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Привет, сервер!")  # Отправляем сообщение
+
+        for _ in range(5):
+            response = await websocket.recv()  # Получаем ответ от сервера
+            print(response)
+
+
+asyncio.run(client())
