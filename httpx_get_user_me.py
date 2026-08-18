d0b531541963d91fabeb4e2414044522784528af
VEKul31 <Sev7715@gmail.com>
2026-08-18 18:04:19 +0300

    Введение в HTTPX 3.0

---
+++ httpx_get_user_me.py
@@ -0,0 +1,21 @@
+import httpx
+
+
+# Инициализируем JSON-данные, которые будем отправлять в API
+login_payload = {
+    "email": "VEK@example.com",
+    "password": "password"
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+get_user_headers = {
+    "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+get_user_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=get_user_headers)
+get_user_response_data = get_user_response.json()
+
+# Выводим обновленные токены
+print("Get user response:", get_user_response_data)
+print("Get user status code:", get_user_response.status_code)
