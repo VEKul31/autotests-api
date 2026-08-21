05451bac30750df12b1bd8df377bd2e19f642bab
VEKul31 <Sev7715@gmail.com>
2026-08-21 13:20:41 +0300

    Практикуемся в написании API автотестов. Часть 1

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -28,3 +28,10 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения файла.
+    """
+    file: FileSchema
