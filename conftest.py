6c9c3a1d24a446a75d238104199f41631c31d082
VEKul31 <Sev7715@gmail.com>
2026-08-21 12:18:40 +0300

    Рефакторинг и подготовка к написанию API автотестов

--- conftest.py
+++ conftest.py
@@ -1,4 +1,10 @@
+import pytest
+pytest.register_assert_rewrite('fixtures.users')
+
 pytest_plugins = (
     "fixtures.authentication",
+    "fixtures.files",
+    "fixtures.courses",
+    "fixtures.exercises",
     "fixtures.users"
 )
