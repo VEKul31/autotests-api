8de4dae280bb28c2321d9b5a52de1dd303c9c690
VEKul31 <Sev7715@gmail.com>
2026-08-24 13:07:39 +0300

    Добавление Allure шагов в автотесты

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,6 +1,8 @@
 from typing import Any, Sized
 
+import allure
 
+@allure.step("Check that response status code equals to {expected}")
 def assert_status_code(actual: int, expected: int):
     """
     Проверяет, что фактический статус-код ответа соответствует ожидаемому.
@@ -15,6 +17,7 @@ def assert_status_code(actual: int, expected: int):
         f'Actual status code: {actual}'
     )
 
+@allure.step("Check that {name} equals to {expected}")
 def assert_equal(actual: Any, expected: Any, name: str):
     """
     Проверяет, что фактическое значение равно ожидаемому.
@@ -30,6 +33,7 @@ def assert_equal(actual: Any, expected: Any, name: str):
         f'Actual value: {actual}'
     )
 
+@allure.step("Check that {name} is true")
 def assert_is_true(actual: Any, name: str):
     """
     Проверяет, что фактическое значение является истинным.
