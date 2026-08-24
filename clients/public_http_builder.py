d5010254400edea8f2f91a7bd88cba91ec0945d2
VEKul31 <Sev7715@gmail.com>
2026-08-24 16:20:30 +0300

    Добавил логирование для API автотестов

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,6 +1,6 @@
 from httpx import Client
 
-from clients.event_hooks import curl_event_hook
+from clients.event_hooks import curl_event_hook, log_request_event_hook, log_response_event_hook
 
 
 def get_public_http_client() -> Client:
@@ -12,5 +12,8 @@ def get_public_http_client() -> Client:
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        event_hooks={"request": [curl_event_hook]}
+        event_hooks={
+            "request": [curl_event_hook, log_request_event_hook],  # Логируем исходящие HTTP-запросы
+            "response": [log_response_event_hook]  # Логируем полученные HTTP-ответы
+        }
     )
